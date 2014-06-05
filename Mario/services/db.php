9b4786cc0c94d2a9dd3222a44d87bd46eacba449
jlangowski <root@Jason-Langowskis-MacBook-Pro.local>
2014-06-05 13:06:42 -0700

    changed db connection again

--- Mario/services/db.php
+++ Mario/services/db.php
@@ -1,4 +1,4 @@
 <?php
- 	$db = new PDO('mysql:host=97.74.31.7;dbname=Mario;port=3306','mariogame6673299', 'hjer987342jKs#');
+ 	$db = new PDO('mysql:host=97.74.31.7;dbname=mariogame6673299;port=3306','mariogame6673299', 'hjer987342jKs#');
 	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 ?>
